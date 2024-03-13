05310d1b210a7b5f0b71ae9f82c135e8c92d4d40
DJ-Man-2099 <davidmesak@hotmail.com>
2024-03-13 04:24:28 +0200

    Dang it

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBrltC5gGJD2W_NK-N8aiGCsH_GEtsfWXM',
+    authDomain: 'rona-sp.firebaseapp.com',
+    projectId: 'rona-sp',
+    storageBucket: 'rona-sp.appspot.com',
+    messagingSenderId: '657318687141',
+    appId: '1:657318687141:web:737b0b09b919c955fc384c',
+  },
+  user: {
+    username: 'test@test.com',
+    password: 'test2024',
+  }
+};
